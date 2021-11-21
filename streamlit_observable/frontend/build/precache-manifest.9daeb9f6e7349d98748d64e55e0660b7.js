42be5b878730ec6be80d4f04d12dd5558f0b827f
SaulLu <lucilesaul.com@gmail.com>
2021-11-21 22:42:53 +0100

    add static component

---
+++ streamlit_observable/frontend/build/precache-manifest.9daeb9f6e7349d98748d64e55e0660b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c599c31bab6b719c258351791f355c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd4500d3447438bcd3ae",
+    "url": "./static/js/2.c66b223a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.c66b223a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d156ecb2c0270aab4219",
+    "url": "./static/js/main.29a7bf42.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.29a7bf42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);

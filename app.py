2e0cc12b85433953df978f2ad4ac5977533efe77
SaulLu <lucilesaul.com@gmail.com>
2021-11-21 23:37:01 +0100

    change order

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dashboard_utils.bubbles import get_new_bubble_data
 wandb.login(anonymous="must")
 
 st.title("Training transformers together dashboard")
-st.header("Training Loss")
+st.caption("Training Loss")
 
 steps, losses, alive_peers = get_main_metrics()
 source = pd.DataFrame({
@@ -21,21 +21,22 @@ chart_loss = alt.Chart(source).mark_line().encode(
     x='steps',
     y='loss'
 )
-st.altair_chart(chart_loss)
+st.altair_chart(chart_loss,  use_container_width=True)
 
-st.header("Collaborative training participants")
-st.header("Snapshot")
-serialized_data, profiles = get_new_bubble_data()
-observers = observable(
-    "Participants",
-    notebook="d/9ae236a507f54046",  # "@huggingface/participants-bubbles-chart",
-    targets=["c_noaws"],
-    redefine={"serializedData": serialized_data, "profileSimple": profiles},
-)
-
-st.header("Overtime")
+st.caption("Number of alive participants over time")
 chart_alive_peer = alt.Chart(source).mark_line().encode(
     x='steps',
     y='alive participants'
 )
-st.altair_chart(chart_alive_peer)
+st.altair_chart(chart_alive_peer,  use_container_width=True)
+
+st.header("Collaborative training participants")
+serialized_data, profiles = get_new_bubble_data()
+with st.spinner('Wait for it...'):
+    observers = observable(
+        "Participants",
+        notebook="d/9ae236a507f54046",  # "@huggingface/participants-bubbles-chart",
+        targets=["c_noaws"],
+        redefine={"serializedData": serialized_data, "profileSimple": profiles},
+    )
+
